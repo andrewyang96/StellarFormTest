41ff798290098541b0d9a0f8ed319c8029b59ad6
Andrew Yang <andrewyang96@gmail.com>
2018-02-01 18:50:57 -0600

    Construct transaction

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,31 @@
 (function() {
+  var getSequenceNumber = function (publicKey, cb) {
+    var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+    server.loadAccount(publicKey).then(function (result) {
+      cb(result.sequenceNumber());
+    });
+  };
+
   document.getElementById('form').onsubmit = function (e) {
     e.preventDefault();
     var destinationAddress = this.querySelector('[name=destination]').value;
     var amount = this.querySelector('[name=amount]').value;
-    var memoField = this.querySelector('[name=memo]').value || null;
+    var memoText = this.querySelector('[name=memo]').value || null;
     var sourceSeed = this.querySelector('[name=sourceSeed]').value;
+
+    // Build transaction
+    StellarSdk.Network.useTestNetwork();
+    getSequenceNumber(destinationAddress, function (sequenceNumber) {
+      var account = new StellarSdk.Account(destinationAddress, sequenceNumber);
+      var memo = memoText ? StellarSdk.Memo.text(memoText) : StellarSdk.Memo.none();
+      var transaction = new TransactionBuilder(account, {memo: memo})
+        .addOperation(StellarSdk.Operation.payment({
+            destination: destinationAddress,
+            asset: StellarBase.Asset.native(),
+            amount: amount
+          }))
+        .build();
+      // TODO: sign
+    });
   };
 })();
