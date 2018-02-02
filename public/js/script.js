1d318a4fcd45689528d9fd49a23c72c41e0a1b8d
Andrew Yang <andrewyang96@gmail.com>
2018-02-01 18:32:55 -0600

    Memo field

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,7 @@
     e.preventDefault();
     var destinationAddress = this.querySelector('[name=destination]').value;
     var amount = this.querySelector('[name=amount]').value;
+    var memoField = this.querySelector('[name=memo]').value || null;
     var sourceSeed = this.querySelector('[name=sourceSeed]').value;
   };
 })();
